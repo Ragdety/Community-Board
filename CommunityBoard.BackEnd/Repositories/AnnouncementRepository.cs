9f4c4892b52c672ca7f841a76e1039f171b53c5b
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-02-24 11:33:05 -0500

    Implemented interface stubs

--- CommunityBoard.BackEnd/Repositories/AnnouncementRepository.cs
+++ CommunityBoard.BackEnd/Repositories/AnnouncementRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityBoard.Core.Interfaces;
+using CommunityBoard.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,39 @@ namespace CommunityBoard.BackEnd.Repositories
 {
     public class AnnouncementRepository : IAnnouncementRepository
     {
+        public Task<Announcement> CreateAsync(Announcement entity)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<Announcement> DeleteAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Announcement> DeleteUserAnnouncement(int announcementId, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Announcement>> FindAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Announcement> FindAllUserAnnouncements(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Announcement> FindAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- CommunityBoard.BackEnd/Repositories/ReportRepository.cs
+++ CommunityBoard.BackEnd/Repositories/ReportRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityBoard.Core.Interfaces;
+using CommunityBoard.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,29 @@ namespace CommunityBoard.BackEnd.Repositories
 {
     public class ReportRepository : IReportRepository
     {
+        public Task<Report> CreateAsync(Report entity)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<Report> DeleteAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Report>> FindAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Report> FindAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
