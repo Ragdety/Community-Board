306951371f946720e831772636264c83efcd8349
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-02-24 11:33:30 -0500

    Added Db Context & connection string

---
+++ CommunityBoard.BackEnd/Database/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CommunityBoard.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommunityBoard.BackEnd.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public ApplicationDbContext() { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Report> Reports { get; set; }
+    }
+}
