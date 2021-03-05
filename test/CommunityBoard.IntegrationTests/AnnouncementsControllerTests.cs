40bebc23996e2ca03d31847b15a43c8899da58f4
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-03-04 23:25:03 -0500

    Implemented 2 integration tests

---
+++ test/CommunityBoard.IntegrationTests/AnnouncementsControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using CommunityBoard.BackEnd.Contracts.V1;
+using CommunityBoard.Core.DTOs;
+using CommunityBoard.Core.Enums;
+using CommunityBoard.Core.Models;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommunityBoard.IntegrationTests
+{
+    public class AnnouncementsControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithAnnouncements_ReturnsList()
+        {
+            //Arrange
+            await AuthenticateAsync();
+
+            //Act
+            var response = await TestClient.GetAsync(ApiRoutes.Announcements.GetAll);
+            var content = await response.Content.ReadAsAsync<List<Announcement>>();
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            content.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsAnnouncement_WhenExist()
+        {
+            //Arrange
+            await LoginAsync();
+            var createdAnnouncement = await CreateAnnouncementAsync(new CreateAnnouncementDto
+            {
+                Name = "Test Announcement",
+                Type = "Sale",
+                Description = "Test Description",
+                Image = null
+            });
+
+            //Act
+            var response = await TestClient.GetAsync(ApiRoutes.Announcements.Get.Replace(
+                    "{announcementId}", createdAnnouncement.Id.ToString()));
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var returnedAnnouncement = await response.Content.ReadAsAsync<Announcement>();
+            returnedAnnouncement.Id.Should().Be(createdAnnouncement.Id);
+            returnedAnnouncement.Name.Should().Be("Test Announcement");
+            returnedAnnouncement.Type.Should().Be(AnnouncementType.Sale);
+            returnedAnnouncement.Description.Should().Be("Test Description");
+            returnedAnnouncement.Image.Should().BeNull();
+        }
+    }
+}
---
+++ test/CommunityBoard.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,88 @@
+﻿using CommunityBoard.BackEnd;
+using CommunityBoard.BackEnd.Contracts.V1;
+using CommunityBoard.BackEnd.Data;
+using CommunityBoard.Core.DTOs;
+using CommunityBoard.Core.DTOs.Responses;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System;
+
+namespace CommunityBoard.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(ApplicationDbContext));
+                        services.AddDbContext<ApplicationDbContext>(options =>
+                        {
+                            //Use this for testing purposes
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = 
+                new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        protected async Task LoginAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("bearer", await GetJwtLoginAsync());
+        }
+
+        protected async Task<AnnouncementResponse> CreateAnnouncementAsync(
+            CreateAnnouncementDto createAnnouncementDto)
+        {
+            var res = await TestClient.PostAsJsonAsync(ApiRoutes.Announcements.Create, createAnnouncementDto);
+            return await res.Content.ReadAsAsync<AnnouncementResponse>();
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync(
+                ApiRoutes.Identity.Register, new UserRegistrationDto
+                {
+                    FirstName = "JohnTest",
+                    LastName = "DoeTest",
+                    UserName = "JohnDoeTest",
+                    Email = "test@integration.com",
+                    Password = "Test1234",
+                    DateRegistered = DateTime.UtcNow
+                });
+
+            var registraionResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+            return registraionResponse.Token;
+        }
+
+        private async Task<string> GetJwtLoginAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync(
+                ApiRoutes.Identity.Login, new UserLoginDto
+                {
+                    EmailOrUserName = "JohnDoeTest",
+                    Password = "Test1234",
+                });
+
+            var registraionResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+            return registraionResponse.Token;
+        }
+    }
+}
